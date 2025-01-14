e772dd6455c8981ed9b0d487b16331c8d64c5a33
madhawaQbitum <madhawa@qbitum.com>
2025-01-14 20:22:55 +0530

    change api service url

--- lib/main_view/api_service.dart
+++ lib/main_view/api_service.dart
@@ -4,15 +4,13 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   static const String baseUrl =
-      'https://run.mocky.io/v3/4f9dbc3f-53c0-4b5d-8631-976a9a3c07f8'; // Example URL
-
-  static Future<Map<String, dynamic>?> fetchImageData() async{
+      'https://run.mocky.io/v3/83849319-3ac3-41a8-9c17-6a276235e925';
+  static Future<Map<String, dynamic>?> fetchImageData() async {
     try {
       final response = await http.get(Uri.parse(baseUrl));
 
       if (response.statusCode == 200) {
         return jsonDecode(response.body);
-
       } else {
         log('Failed to fetch data. Status code: ${response.statusCode}');
         return null;
